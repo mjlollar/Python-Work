afd2db174bb01563b4d1c8ed322b5a4613e0ea27
Matthew Lollar <mjlollar1@gmail.com>
2016-09-30 09:15:41 -0500

    Update GC_Content_Test.py

--- GC_Content_Test.py
+++ GC_Content_Test.py
@@ -1,23 +1,22 @@
-import sys
+#!/usr/bin/env python
 
-# This script serves the purpose of testing primers for efficacy using a realtively 
-# simple algorithm.
+print('Welcome to the Primer GC content test. Please enter the primer you wish to test')
 
-print('Welcome to the Primer GC content test. Please enter or the primer you wish to test or enter "README" for more information.')
+primer_entry = input("Paste sequence here:\n").upper()
 
-while True:
-	primer_entry = input("Paste sequence here:\n").upper()
-	if primer_entry in ['README']:
-		print("More information coming soon")
-	else:
-	    break	
-def validate_base_sequence(primer_entry):
-    seq = primer_entry
-    assert len(seq) == seq.count('U') + seq.count('C') + seq.count('A') + seq.count('G'), "Sequence is invalid, please enter a valid sequence"
-def gc_content(primer_entry):
-    seq = primer_entry
-    return (seq.count('G') + seq.count('C')) / len(seq)
+
+seq = primer_entry
+assert len(seq) == (seq.count('U') + seq.count('C') + seq.count('A') + seq.count('G')), "Sequence is invalid, please enter a valid sequence" 
 	
+gc_content = float((seq.count('G') + seq.count('C')) / len(seq)) * 100
 
 print("Your GC content is:\n")
-print(gc_content(primer_entry))
+print(str(gc_content) + "%")
+
+while True:
+    if gc_content >= 50.0:
+        print("This GC content is within acceptable range!")
+        break
+    else:
+        print("This GC content is too low!")
+        break
