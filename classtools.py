aab3d6b489765225636f95d92d98d54996bece92
Matthew Lollar <mjlollar1@gmail.com>
2017-07-25 12:40:14 -0500

    Update classtools.py

--- classtools.py
+++ classtools.py
@@ -1,29 +1,29 @@
 class AttrDisplay:
 	"""
-	Inheritable display overload that shows instances with 
+    Inheritable display overload method that shows instances with 
     their class names and a name=value pair for each attribute
-    stored on the instance itself. Can be mized into any class,
+    stored on the instance itself. Can be mixed into any class,
     and works on any instance. Does not display attributes
-	inherited from classes.
+    inherited from classes.
 	"""
 	
 	def gathersAttrs(self):
-			attrs = []
+		attrs = []
 		for key in sorted(self.__dict__):
-			attrs.append('%s=%s' % (key, getattr(self, key)))
-			return ','.join(attrs)
+		attrs.append('%s=%s' % (key, getattr(self, key)))
+		return ','.join(attrs)
 			
 	def __repr__(self):
 		return '[%s: %s]' % (self.__class__.__name__, self.gatherAttrs())
 		
 if __name__ == '__main__':
-	
 	class TopTest(AttrDisplay):
 		count = 0
 		def __init__(self):
 			self.attr1 = TopTest.count
 			self.attr2 = TopTest.count+1
 			TopTest.count += 2
+
 	class SubTest(TopTest):
 		pass
 	
