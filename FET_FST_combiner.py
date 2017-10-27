7821425d1d4be47171f21e2ef91861d901063feb
Matthew Lollar <mjlollar1@gmail.com>
2017-10-26 20:39:21 -0500

    Create FET_FST_combiner.py
    Should be final version of this script.  Works fine, but there is an artifact in the CSV file that could be removed (parenthesis in the two columns of the key tuple). Can't seem to find a way to remove these without errors in the script, but they don't matter much outside of aesthetic appeal of the CSV file.  Won't matter for my personal analysis, but if file wanted more global use in the future is a problem I might want to address.

---
+++ FET_FST_combiner.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+import csv
+
+csv.register_dialect('Comma_Delin', delimiter=',', quoting=csv.QUOTE_NONE)
+
+#This is designed to work with output files of "LD_between_loci_filter_strongest_*.pl" files.
+
+file_name1 = input("Insert LD FET file name:\n")
+
+with open(file_name1, 'r') as File1:
+	reader = csv.reader(File1, dialect='Comma_Delin')
+	dict_FET = {}
+	for rows in reader:
+		try:
+			key = (int(rows[1]), int(rows[0]))
+			dict_FET[key] = rows[3]	
+		except ValueError:
+			for key in rows:
+				del key
+			continue
+print(dict_FET)
+			
+print("FET values for" + " " + str(file_name1) + " " + "compiled.")
+	
+#This is designed to work with output files of "FST_*_Chr*.pl".
+
+file_name2 = input("Insert FST file name:\n")
+
+with open(file_name2, 'r') as File2:
+	reader = csv.reader(File2, dialect='Comma_Delin')
+	dict_FST = {}
+	for rows in reader:
+		try:
+			row1 = int(rows[1])
+			row2 = int(rows[2])
+			key = range(row1, row2)
+			dict_FST[key] = rows[4]
+		except ValueError:
+				for key in rows:
+					del[key]
+				continue
+				
+print("FST values and SNP positions for" + " " + str(file_name2) + " " + "compiled")
+
+dict_combined = {}
+
+for key, value1 in dict_FET.items():
+    dict_combined.setdefault(key, [value1]).extend(
+        value2 for interval, value2 in dict_FST.items() if key[0] in interval)
+
+new_file = input("Insert a file name for your new file, include .csv:\n")
+
+with open(new_file, 'w', newline='') as File3:
+	writer = csv.writer(File3, dialect='Comma_Delin', escapechar=' ')
+	writer.writerow(["Fife Pos", "SNP Pos", "FETP", "FST"])
+	writer.writerows([row[0]] + row[1] for row in dict_combined.items())
+
+print(str(new_file) + ' ' + "has been created in your current directory.")
